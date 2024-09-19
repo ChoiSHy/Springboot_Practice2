283c8569c54e78795f2c533b20989f3f0db71029
ChoiSHy <tjdgus4697@gmail.com>
2024-09-20 03:29:57 +0900

    데이터베이스 활용 CRUD 기능으로 변경

--- Product-management/src/main/java/kr/co/hanbit/productmanagement/domain/Product.java
+++ Product-management/src/main/java/kr/co/hanbit/productmanagement/domain/Product.java
@@ -3,13 +3,17 @@ package kr.co.hanbit.productmanagement.domain;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.Size;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 import lombok.ToString;
 
 import java.util.Objects;
 
 @ToString
 @NoArgsConstructor
+@Getter
+@Setter
 public class Product {
 
     private Long id;
@@ -27,11 +31,6 @@ public class Product {
         this.price = price;
         this.amount = amount;
     }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public Boolean sameId(Long id) {
         // Getter 없이 이름 비교를 위해 존재
         return this.id.equals(id);
@@ -41,7 +40,6 @@ public class Product {
         // Getter 없이 이름 비교를 위해 존재
         return this.name.contains(name);
     }
-
     @Override
     public boolean equals(Object obj) {
         if (this == obj)
@@ -50,17 +48,5 @@ public class Product {
             return false;
         Product product = (Product) obj;
         return Objects.equals(id, product.id);  // 두 객체의 id만 같아도 같은 정보라고 인식
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public Integer getPrice() {
-        return price;
-    }
-
-    public Integer getAmount() {
-        return amount;
     }
 }
--- Product-management/src/main/java/kr/co/hanbit/productmanagement/infrastructure/DatabaseProductRepository.java
+++ Product-management/src/main/java/kr/co/hanbit/productmanagement/infrastructure/DatabaseProductRepository.java
@@ -3,9 +3,11 @@ package kr.co.hanbit.productmanagement.infrastructure;
 import kr.co.hanbit.productmanagement.domain.Exception.EntityNotFoundException;
 import kr.co.hanbit.productmanagement.domain.Product;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.SqlParameter;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.jdbc.support.GeneratedKeyHolder;
@@ -46,22 +48,53 @@ public class DatabaseProductRepository {
     }
 
     public Product findById(Long id) {
-        return null;
+        SqlParameterSource namedParameter = new MapSqlParameterSource("id",id); // id 매핑을 위해 MapSqlParameterSource 사용
+        Product product = namedJdbcTemplate.queryForObject(
+                "select id, name, price, amount from products where id = :id",   // select문으로 조건에 맞는 데이터 불러오기
+                namedParameter,
+                new BeanPropertyRowMapper<>(Product.class)                          // 조회한 정보를 Product 인스턴스로 만들어주기
+        );                                                                          // 필요한 조건: 클래스에 인자없는 생성자 + 각 속성에 대한 setter 메서드
+
+        return product;
     }
 
     public List<Product> findAll() {
-        return null;
+        List<Product> products = namedJdbcTemplate.query(
+                "select * from products",
+                new BeanPropertyRowMapper<>(Product.class)
+        );
+
+        return products;
     }
 
     public List<Product> findByName(String name) {
-        return null;
+        SqlParameterSource namedParameter = new MapSqlParameterSource("name", "%"+name+"%");
+
+        List<Product> products = namedJdbcTemplate.query(
+                "select * from products where name like :name"
+                ,namedParameter
+                ,new BeanPropertyRowMapper<>(Product.class)
+        );
+
+        return products;
     }
 
     public Product update(Product product) {
-        return null;
+        SqlParameterSource namedParameter = new BeanPropertySqlParameterSource(product);
+        namedJdbcTemplate.update(
+                "update products set name=:name, price=:price, amount=:amount where id=:id"
+        ,namedParameter);
+
+        return product;
     }
 
     public void delete(Long id) {
+        SqlParameterSource namedParameter = new MapSqlParameterSource("id", id);
+
+        namedJdbcTemplate.update(
+                "delete from products where id=:id",
+                namedParameter
+        );
     }
 
 }
